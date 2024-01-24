430cae48073f8131e7d70a2cad51432706655c68
NotYourFridge <romanopoeran@gmail.com>
2024-01-24 01:05:29 +0100

    d

--- server/index.js
+++ server/index.js
@@ -1,26 +1,21 @@
 const express = require("express");
 const app = express();
 const http = require("http");
-/*
-Boventste 3 heb je nodig om onze server samen te bouwen met socket.io
-*/
 const cors = require("cors");
 const { Server } = require("socket.io");
-const { Socket } = require("dgram");
+
 app.use(cors());
+
 const server = http.createServer(app);
+
+const CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || "*"; // Set the default value to "*"
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000", //welke url/server(react server) een call doet naar de socket.io server
+    origin: CLIENT_ORIGIN,
     methods: ["GET", "POST"],
   },
 });
-
-/* io.on();
-initialiseer an detecteer of iemand verbonden is met de server 
-luisteren of er een event is met een bepaalde id of naam.
-*/
 
 io.on("connection", (socket) => {
   console.log(`user connected: ${socket.id}`);
@@ -31,7 +26,7 @@ io.on("connection", (socket) => {
   });
 
   socket.on("send_message", (data) => {
-      socket.to(data.room).emit("recieve_message",data)
+    socket.to(data.room).emit("recieve_message", data);
   });
 
   socket.on("disconnect", () => {
@@ -39,6 +34,8 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(3001, () => {
-  console.log("[server running]");
+const PORT = process.env.PORT || 3001;
+
+server.listen(PORT, () => {
+  console.log(`[server running on port ${PORT}]`);
 });
